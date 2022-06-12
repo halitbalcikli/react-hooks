2cdf431e6092c2ed2493da7c8fd2251e2be99fa3
halitbalcikli <halit_balcikli@hotmail.com>
2022-06-12 21:41:59 +0300

    dropdown from zero

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import { useState } from 'react';
 
 const items = [
   {
@@ -15,12 +17,33 @@ const items = [
     title: 'How do you use React',
     content: 'You use React by creating components'
   },
-]
+];
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+];
 
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown 
+        selected={selected} 
+        onSelectedChange={setSelected}
+        options={options} 
+      />
     </div>
   );
 }
